9f6fb1f488d8c43b044ba2fe39aa335876590465
viniciusofp <viniciusofp@gmail.com>
2026-02-11 19:28:27 -0300

    compare

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -17,6 +17,7 @@ import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import Fade, { FadeType } from 'embla-carousel-fade';
+import WheelGesturesPlugin from 'embla-carousel-wheel-gestures';
 
 export type pageProps = {};
 
@@ -38,6 +39,9 @@ export default function page(props: pageProps) {
       return;
     }
     api.on('select', () => setCurr(api.selectedScrollSnap()));
+    api.on('scroll', (e) => {
+      console.log(e);
+    });
   }, [api]);
 
   const scrollPrev = useCallback(() => {
@@ -52,47 +56,49 @@ export default function page(props: pageProps) {
     }
   }, [api]);
   return (
-    <div className="relative h-svh w-full bg-black overflow-hidden">
-      <div className="absolute top-4 left-4 text-white  z-5">
-        <h1 className="text-shadow-lg md:text-xl tracking-wider leading-relaxed mb-1 text-balance">
-          Selecione uma data para ver o impacto <br />
-          da Braskem sobre a região:
-        </h1>
-        <div className="flex gap-5 flex-wrap ">
-          {images.map((imgArr: [string, StaticImageData], index) => {
-            const date = new Date(imgArr[0]);
-            return (
-              <button
-                key={imgArr[0]}
-                className={cn(
-                  'opacity-100 cursor-pointer duration-150 text-left leading-none! text-3xl md:text-4xl font-black text-shadow-lg border-b-2 pb-1 text-[#29abe2] border-transparent',
-                  curr === index &&
-                    'opacity-100 border-b-2 border-white text-white'
-                )}
-                onClick={() => {
-                  setCurr(index);
-                  api?.scrollTo(index);
-                }}
-                onMouseEnter={() => {
-                  setCurr(index);
-                  api?.scrollTo(index);
-                }}
-              >
-                <span className="text-sm uppercase font-medium">
+    <>
+      <div className="h-96 bg-stone-300 m-8 rounded-xl aspect-square"></div>
+      <div className="relative h-svh w-full bg-black overflow-hidden">
+        <div className="absolute top-4 left-4 text-white  z-5">
+          <h1 className="text-shadow-lg md:text-xl tracking-wider leading-relaxed mb-1 text-balance">
+            Selecione uma data para ver o impacto <br />
+            da Braskem sobre a região:
+          </h1>
+          <div className="flex gap-5 flex-wrap ">
+            {images.map((imgArr: [string, StaticImageData], index) => {
+              const date = new Date(imgArr[0]);
+              return (
+                <button
+                  key={imgArr[0]}
+                  className={cn(
+                    'opacity-100 cursor-pointer duration-150 text-left leading-none! text-3xl md:text-4xl font-black text-shadow-lg border-b-2 pb-1 text-[#29abe2] border-transparent',
+                    curr === index &&
+                      'opacity-100 border-b-2 border-white text-white'
+                  )}
+                  onClick={() => {
+                    setCurr(index);
+                    api?.scrollTo(index);
+                  }}
+                  onMouseEnter={() => {
+                    setCurr(index);
+                    api?.scrollTo(index);
+                  }}
+                >
+                  <span className="text-sm uppercase font-medium">
+                    {date.toLocaleDateString('pt-BR', {
+                      month: 'long'
+                    })}{' '}
+                    DE
+                  </span>
+                  <br />
                   {date.toLocaleDateString('pt-BR', {
-                    month: 'long'
-                  })}{' '}
-                  DE
-                </span>
-                <br />
-                {date.toLocaleDateString('pt-BR', {
-                  year: 'numeric'
-                })}
-              </button>
-            );
-          })}
-        </div>
-        {/* 
+                    year: 'numeric'
+                  })}
+                </button>
+              );
+            })}
+          </div>
+          {/* 
         <p className="text-xs mt-8 font-medium text-white text-shadow">
           Fonte das Imagens:{' '}
           <Link
@@ -103,36 +109,38 @@ export default function page(props: pageProps) {
             Esri World Imagery Wayback
           </Link>
         </p> */}
-      </div>
-      <div className="absolute z-3 left-0 top-0 w-full h-full bg-linear-to-br from-black/80 via-transparent via-60% to-transparent mix-blend-hardlight pointer-events-none"></div>
+        </div>
+        <div className="absolute z-3 left-0 top-0 w-full h-full bg-linear-to-br from-black/80 via-transparent via-60% to-transparent mix-blend-hardlight pointer-events-none"></div>
 
-      <Carousel
-        setApi={setApi}
-        plugins={[Fade()]}
-        opts={{ loop: true }}
-        className="w-full h-svh"
-      >
-        <CarouselContent className="w-full h-full ml-0">
-          {images.map((imgArr: [string, StaticImageData], index) => {
-            return (
-              <CarouselItem
-                key={imgArr[0]}
-                className="relative left-0 top-0 w-full h-svh pl-0"
-              >
-                <img
-                  className="object-center object-cover w-full h-full "
-                  src={imgArr[1].src}
-                  alt={imgArr[0]}
-                  width={1980}
-                  height={1303}
-                />
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-        {/* <CarouselPrevious />
+        <Carousel
+          setApi={setApi}
+          plugins={[Fade()]}
+          opts={{ loop: true }}
+          className="w-full h-svh"
+        >
+          <CarouselContent className="w-full h-full ml-0">
+            {images.map((imgArr: [string, StaticImageData], index) => {
+              return (
+                <CarouselItem
+                  key={imgArr[0]}
+                  className="relative left-0 top-0 w-full h-svh pl-0"
+                >
+                  <img
+                    className="object-center object-cover w-full h-full "
+                    src={imgArr[1].src}
+                    alt={imgArr[0]}
+                    width={1980}
+                    height={1303}
+                  />
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          {/* <CarouselPrevious />
           <CarouselNext /> */}
-      </Carousel>
-    </div>
+        </Carousel>
+      </div>
+      <div className="h-96 bg-stone-300 m-8 rounded-xl aspect-square"></div>
+    </>
   );
 }
